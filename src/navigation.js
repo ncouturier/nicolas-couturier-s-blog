46fd5927c8bbe481ead5b6014cde56e38b16e69c
nicolas couturier <n.couturier@gmail.com>
2024-08-17 17:05:47 +0200

    removed unused data

--- src/navigation.js
+++ src/navigation.js
@@ -120,57 +120,57 @@ export const headerData = {
 
 export const footerData = {
   links: [
-    {
-      title: 'Product',
-      links: [
-        { text: 'Features', href: '#' },
-        { text: 'Security', href: '#' },
-        { text: 'Team', href: '#' },
-        { text: 'Enterprise', href: '#' },
-        { text: 'Customer stories', href: '#' },
-        { text: 'Pricing', href: '#' },
-        { text: 'Resources', href: '#' },
-      ],
-    },
-    {
-      title: 'Platform',
-      links: [
-        { text: 'Developer API', href: '#' },
-        { text: 'Partners', href: '#' },
-        { text: 'Atom', href: '#' },
-        { text: 'Electron', href: '#' },
-        { text: 'AstroWind Desktop', href: '#' },
-      ],
-    },
-    {
-      title: 'Support',
-      links: [
-        { text: 'Docs', href: '#' },
-        { text: 'Community Forum', href: '#' },
-        { text: 'Professional Services', href: '#' },
-        { text: 'Skills', href: '#' },
-        { text: 'Status', href: '#' },
-      ],
-    },
-    {
-      title: 'Company',
-      links: [
-        { text: 'About', href: '#' },
-        { text: 'Blog', href: '#' },
-        { text: 'Careers', href: '#' },
-        { text: 'Press', href: '#' },
-        { text: 'Inclusion', href: '#' },
-        { text: 'Social Impact', href: '#' },
-        { text: 'Shop', href: '#' },
-      ],
-    },
+    /* {
+       title: 'Product',
+       links: [
+         { text: 'Features', href: '#' },
+         { text: 'Security', href: '#' },
+         { text: 'Team', href: '#' },
+         { text: 'Enterprise', href: '#' },
+         { text: 'Customer stories', href: '#' },
+         { text: 'Pricing', href: '#' },
+         { text: 'Resources', href: '#' },
+       ],
+     },
+     {
+       title: 'Platform',
+       links: [
+         { text: 'Developer API', href: '#' },
+         { text: 'Partners', href: '#' },
+         { text: 'Atom', href: '#' },
+         { text: 'Electron', href: '#' },
+         { text: 'AstroWind Desktop', href: '#' },
+       ],
+     },
+     {
+       title: 'Support',
+       links: [
+         { text: 'Docs', href: '#' },
+         { text: 'Community Forum', href: '#' },
+         { text: 'Professional Services', href: '#' },
+         { text: 'Skills', href: '#' },
+         { text: 'Status', href: '#' },
+       ],
+     },
+     {
+       title: 'Company',
+       links: [
+         { text: 'About', href: '#' },
+         { text: 'Blog', href: '#' },
+         { text: 'Careers', href: '#' },
+         { text: 'Press', href: '#' },
+         { text: 'Inclusion', href: '#' },
+         { text: 'Social Impact', href: '#' },
+         { text: 'Shop', href: '#' },
+       ],
+     },*/
   ],
   secondaryLinks: [
-    { text: 'Terms', href: getPermalink('/terms') },
-    { text: 'Privacy Policy', href: getPermalink('/privacy') },
+    //{ text: 'Terms', href: getPermalink('/terms') },
+    //{ text: 'Privacy Policy', href: getPermalink('/privacy') },
   ],
   socialLinks: [
-    { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
+    //  { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
     { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
     { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
     { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
